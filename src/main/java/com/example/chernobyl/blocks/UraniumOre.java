dfdbc5a603b8e57ff45b0a3b1e7748872e329df1
SamuelGignac <samuelgignacwork@gmail.com>
2023-10-19 19:15:33 -0400

    uranium ore drops

--- src/main/java/com/example/chernobyl/blocks/UraniumOre.java
+++ src/main/java/com/example/chernobyl/blocks/UraniumOre.java
@@ -1,10 +1,12 @@
 package com.example.chernobyl.blocks;
 
 import com.example.chernobyl.Chernobyl;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,13 +15,12 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
-// todo : drop uranium when mined
 public class UraniumOre {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Chernobyl.MODID);
 
     public static final RegistryObject<Block> URANIUM_ORE = registerBlock("uranium_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE))); // todo : custom properties
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DIAMOND_ORE), UniformInt.of(3,6))); // todo : custom properties
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = Chernobyl.BLOCKS.register(name, block);
